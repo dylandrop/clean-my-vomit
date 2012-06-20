b52473abe73ce693e5698a590c54a603dbe78c66
Dylan Drop <dylandrop@gmail.com>
2012-06-19 22:49:46 -0400

    Initial commit

---
+++ clean-my-vomit.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'clean-my-vomit'
+  s.version     = '0.0.0'
+  s.date        = '2012-06-19'
+  s.summary     = "Clean"
+  s.description = "Ruby gem that makes your long, deeply nested hashes easily displayable on command-line."
+  s.authors     = ["Dylan Drop"]
+  s.email       = 'dylandrop@gmail.com'
+  s.files       = ["lib/clean-my-vomit.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/clean-my-vomit'
+end
---
+++ lib/clean-my-vomit.rb
@@ -0,0 +1,46 @@
+module CleanMyVomit
+  def polish(my_hash=self, depth=0)
+    if !reached_max_recursion_depth?(my_hash)
+      my_hash.each do |key, value|
+        if value.is_a? Hash
+          print_hash(key, value, depth)
+        elsif value.is_a? Array
+          print_array(key, value, depth)
+        else
+          if key.is_a? Hash
+            puts " "*depth + " {"
+            polish(key, depth + 3)
+            puts " "*depth + " }"
+          elsif value
+            puts " "*depth + key + " => #{value}"
+          else
+            puts " "*depth + key.to_s + ""
+          end
+          polish(value, depth + 3)
+        end
+      end
+    end
+  end
+
+  private
+  def reached_max_recursion_depth?(my_object)
+    !(my_object.respond_to? :each)
+  end
+
+  # {"3" => { "3" => "2"}}
+  def print_hash(key, value, depth)
+    puts " "*depth + key + " {"
+    polish(value, depth + 3)
+    puts " "*depth + "}"
+  end
+
+  # { "3" => [3,2,3]}
+  def print_array(key, value, depth)
+    puts " "*depth + key + " =>["
+    polish(value, depth + 3)
+    puts " "*depth + "]"
+  end
+end
+
+Hash.send(:include, CleanMyVomit)
+Array.send(:include, CleanMyVomit)
