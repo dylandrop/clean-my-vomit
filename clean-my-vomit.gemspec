7e443abb0bf5f3e37d380e34d6d5de1916b6f195
Dylan Drop <dylandrop@gmail.com>
2012-06-20 21:08:25 -0400

    Fixed the  problem, cleaned up the arrows (=>)

--- clean-my-vomit.gemspec
+++ clean-my-vomit.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'clean-my-vomit'
-  s.version     = '0.0.1'
-  s.date        = '2012-06-19'
+  s.version     = '0.0.3'
+  s.date        = '2012-06-20'
   s.summary     = "Clean"
   s.description = "Ruby gem that makes your long, deeply nested hashes easily displayable in IRB."
   s.authors     = ["Dylan Drop"]
   s.email       = 'dylandrop@gmail.com'
   s.files       = ["lib/clean-my-vomit.rb"]
   s.homepage    =
-    'http://rubygems.org/gems/clean-my-vomit'
+    'https://github.com/dylandrop/clean-my-vomit'
 end
--- lib/clean-my-vomit.rb
+++ lib/clean-my-vomit.rb
@@ -11,8 +11,7 @@ module CleanMyVomit
             puts " "*depth + " {"
             polish(key, depth + 3)
             puts " "*depth + " }"
-          # NOTE: not just `value`, because `value` could be `false`, in which case we want to print it out
-          elsif !value.nil?
+          elsif value || value == false
             puts " "*depth + key + " => #{value.to_s}"
           else
             puts " "*depth + key.to_s + ""
@@ -22,7 +21,7 @@ module CleanMyVomit
       end
     end
   end
-
+  
   private
   def reached_max_recursion_depth?(my_object)
     !(my_object.respond_to? :each)
@@ -30,7 +29,7 @@ module CleanMyVomit
 
   # {"3" => { "3" => "2"}}
   def print_hash(key, value, depth)
-    puts " "*depth + key + " {"
+    puts " "*depth + key + " =>{"
     polish(value, depth + 3)
     puts " "*depth + "}"
   end
